f476d755b73bd0748b5713fe26ab69c882a865ce
milan-codes <github@milancodes.io>
2023-02-05 11:23:20 +0100

    Initial commit

---
+++ src/components/About.svelte
@@ -0,0 +1,13 @@
+<div class="max-w-lg mx-auto px-4">
+    <div class="my-4">
+        <h1 class="text-xl text-gray-900 tracking-wider">Milán Herke</h1>
+        <h1 class="text-gray-600">Software engineer</h1>
+        <h1 class="text-sm text-gray-400">hi@milanherke.dev</h1>
+    </div>
+    <div class="my-8">
+        <h1 class="text-gray-900 mb-2 tracking-wider">About</h1>
+        <p class="text-gray-600">
+            Hi, I'm Milán. I enjoy building creative, cool products. I'm currently working towards obtaining my bachelor's degree in computer science at the University of Pécs, Faculty of Sciences.
+        </p>
+    </div>
+</div>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,16 @@
+<nav aria-label="Navigation bar" class="mx-auto flex max-w-lg items-center justify-between p-4">
+  <a href="/" class="inline-flex px-3 py-1 items-center justify-center rounded-lg bg-gray-100 font-semibold text-gray-500 hover:text-gray-700">
+    <span class="sr-only">Logo</span> 👋
+  </a>
+
+  <ul class="flex items-center gap-2 text-sm font-medium text-gray-500">
+    <li>
+      <a class="inline-flex items-center rounded-lg px-3 py-2 hover:text-gray-700" href="https://github.com/milan-codes" target="_blank" rel="noreferrer">
+        GitHub
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="ml-1.5 h-4 w-4">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</nav>
