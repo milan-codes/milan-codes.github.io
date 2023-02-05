f39acc24447a56cc31c5798013a5914293afa91f
milan-codes <github@milancodes.io>
2023-02-05 18:58:49 +0100

    Basic blog page

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -5,7 +5,17 @@
 
   <ul class="flex items-center gap-2 text-sm font-medium text-gray-900 dark:text-gray-100">
     <li>
-      <a class="inline-flex items-center rounded-lg px-3 py-2 hover:underline" href="https://github.com/milan-codes" target="_blank" rel="noreferrer">
+      <a class="px-3 py-2 hover:underline" href="/">
+        Home
+      </a>
+    </li>
+    <li>
+      <a class="px-3 py-2 hover:underline" href="/blog">
+        Blog
+      </a>
+    </li>
+    <li>
+      <a class="inline-flex items-center pl-2 hover:underline" href="https://github.com/milan-codes" target="_blank" rel="noreferrer">
         GitHub
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ml-1 h-4 w-4"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg>
       </a>
