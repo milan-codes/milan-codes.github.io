3fe3321605aa1d33167965f19f0745ef76796a99
milan-codes <milanherke@protonmail.com>
2023-07-04 10:22:17 +0200

    Added KaTeX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
@@ -8,5 +8,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+  markdown: {
+    remarkPlugins: ["remark-math"],
+    rehypePlugins: ["rehype-katex"],
+  },
 });
