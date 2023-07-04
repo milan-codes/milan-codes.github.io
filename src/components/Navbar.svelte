b989366295819bb77b62b82deb372545d2710ae9
milan-codes <milanherke@protonmail.com>
2023-07-04 13:59:42 +0200

    Added navbar entry for tags

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -15,6 +15,11 @@
       </a>
     </li>
     <li>
+      <a class="px-3 py-2 hover:underline" href="/tags">
+        Tags
+      </a>
+    </li>
+    <li>
       <a class="inline-flex items-center pl-2 hover:underline" href="https://github.com/milan-codes" target="_blank" rel="noreferrer">
         GitHub
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ml-1 h-4 w-4"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg>
--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="my-16">
-  <h1 class="text-gray-900 dark:text-gray-100 mb-2 tracking-wider">Side projects</h1>
+  <h1 class="text-gray-900 dark:text-gray-100 mb-2 tracking-wider">Projects</h1>
   <ProjectCard projectName="Personal blog" projectDescription="The source code of this site" projectLink="https://github.com/milan-codes/milan-codes.github.io" projectTags={["Astro", "Svelte", "Tailwind"]}/>
   <ProjectCard projectName="The Briefing Room" projectDescription="Access and visualize Formula 1 telemetry data" projectLink="https://github.com/milan-codes/the-briefing-room" projectTags={["Next", "React.ts", "Tailwind"]}/>
   <ProjectCard projectName="The Briefing Room API" projectDescription="Simple Flask API to serve the frontend" projectLink="https://github.com/milan-codes/the-briefing-room-api" projectTags={["Flask"]}/>
