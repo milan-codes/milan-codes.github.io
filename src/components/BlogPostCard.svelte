e201f39790e055383bcaeab2909d93a2135fa495
milan-codes <milanherke@protonmail.com>
2023-08-18 09:47:39 +0200

    Changed blog post links to open on the same tab

--- src/components/BlogPostCard.svelte
+++ src/components/BlogPostCard.svelte
@@ -10,8 +10,6 @@
         <a
             class="inline-flex items-center rounded-lg text-gray-900 dark:text-gray-100 hover:underline"
             href={post.url}
-            target="_blank"
-            rel="noreferrer"
         >
             {post.frontmatter.title}
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ml-1 h-4 w-4"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg>
